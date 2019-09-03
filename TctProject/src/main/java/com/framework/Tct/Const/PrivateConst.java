b9e0c2c5345310a312f0f2fa516558880c4ec223
ORA_HOME <37389515+neem693@users.noreply.github.com>
2019-09-04 00:22:30 +0900

    update

--- TctProject/src/main/java/com/framework/Tct/Const/PrivateConst.java
+++ TctProject/src/main/java/com/framework/Tct/Const/PrivateConst.java
@@ -8,7 +8,7 @@ public class PrivateConst {
 		public static final String TOKKEN = "083042e516dfbb519b0942c5d78b4049";
 		public static final String TO_SEND = "+821030435693";
 		public static final String TO_FROM = "+13343266918";
-		
+		테스트
 	}
 
 }
