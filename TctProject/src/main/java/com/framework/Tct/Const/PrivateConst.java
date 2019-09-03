914fb316c891290a07b695923f470db4ebdd2faf
ORA_HOME <37389515+neem693@users.noreply.github.com>
2019-09-04 00:20:08 +0900

    update

--- TctProject/src/main/java/com/framework/Tct/Const/PrivateConst.java
+++ TctProject/src/main/java/com/framework/Tct/Const/PrivateConst.java
@@ -4,10 +4,10 @@ public class PrivateConst {
 	
 	public static class TWILO{
 		
-		public static final String ACCOUNTSID = "";
-		public static final String TOKKEN = "";
-		public static final String TO_SEND = "";
-		public static final String TO_FROM = "";
+		public static final String ACCOUNTSID = "AC5a0271e99af3a7225cf43cfc1ae8b722";
+		public static final String TOKKEN = "083042e516dfbb519b0942c5d78b4049";
+		public static final String TO_SEND = "+821030435693";
+		public static final String TO_FROM = "+13343266918";
 		
 	}
 
