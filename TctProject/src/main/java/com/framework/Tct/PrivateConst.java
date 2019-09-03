b3ca4ddcd9d9477ca524110b2b1c94407ad96e9c
ORA_HOME <37389515+neem693@users.noreply.github.com>
2019-09-03 19:27:41 +0900

    update

---
+++ TctProject/src/main/java/com/framework/Tct/PrivateConst.java
@@ -0,0 +1,12 @@
+package com.framework.Tct;
+
+public class PrivateConst {
+	
+	public static class TWILO{
+		
+		public static final String USERNAME = "";
+		public static final String PASSWORD = "";
+		
+	}
+
+}
