d4ed4c175cd883cf706f144804d85a38d7119cbd
neem693 <37389515+neem693@users.noreply.github.com>
2019-10-29 18:31:54 +0900

    update

--- 인증서 만들기 rsa by git openssl in window/comm.cmd
+++ 인증서 만들기 rsa by git openssl in window/comm.cmd
@@ -1,4 +1,6 @@
-openssl genrsa -out .\jwt.pem 2048
-openssl rsa -in .\jwt.pem
-openssl rsa -in .\jwt.pem -pubout
+C:
+cd C:\Program Files\Git\usr\bin
+openssl genrsa -out C:\key\jwt.pem 2048
+openssl rsa -in C:\key\jwt.pem
+openssl rsa -in C:\key\jwt.pem -pubout
 pause
