e4342a53a7be10e34e6e0b6601c42e502f0ccdfd
neem693 <37389515+neem693@users.noreply.github.com>
2021-05-26 00:15:38 +0900

    update

--- 필요한 튜토리얼/linux/linux.mm
+++ 필요한 튜토리얼/linux/linux.mm
@@ -1,8 +1,73 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1574057360640" ID="ID_726138929" MODIFIED="1574081750620" TEXT="&#xb9ac;&#xb205;&#xc2a4;">
-<node CREATED="1574057446666" ID="ID_776495852" MODIFIED="1574057454592" POSITION="right" TEXT="&#xbc84;&#xcd94;&#xc5b4; &#xbc15;&#xc2a4; &#xc124;&#xce58;">
-<node CREATED="1574062840958" ID="ID_585410409" MODIFIED="1574062841879" TEXT="&#xcc38;&#xc870;">
+<hook NAME="MapStyle">
+<properties EDGECOLORCONFIGURATION="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" FIT_TO_VIEWPORT="false"/>
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode COLOR="#000000" ICON_SIZE="12 pt" ID="ID_271890427" LOCALIZED_TEXT="default" STYLE="fork">
+<arrowlink/>
+<font/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="NOTE"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font/>
+</stylenode>
+<stylenode BACKGROUND_COLOR="#ffffff" COLOR="#000000" LOCALIZED_TEXT="defaultstyle.note" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge/>
+<cloud/>
+</stylenode>
+<stylenode BACKGROUND_COLOR="#4e85f8" BORDER_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" LOCALIZED_TEXT="defaultstyle.selection" STYLE="bubble"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode COLOR="#18898b" LOCALIZED_TEXT="styles.topic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#cc3300" LOCALIZED_TEXT="styles.subtopic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#669900" LOCALIZED_TEXT="styles.subsubtopic">
+<font/>
+</stylenode>
+<stylenode ID="ID_67550811" LOCALIZED_TEXT="styles.important">
+<icon/>
+<arrowlink/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="AutomaticLayout.level.root" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" STYLE="oval">
+<font/>
+</stylenode>
+<stylenode COLOR="#0033ff" LOCALIZED_TEXT="AutomaticLayout.level,1">
+<font/>
+</stylenode>
+<stylenode COLOR="#00b439" LOCALIZED_TEXT="AutomaticLayout.level,2">
+<font/>
+</stylenode>
+<stylenode COLOR="#990000" LOCALIZED_TEXT="AutomaticLayout.level,3">
+<font/>
+</stylenode>
+<stylenode COLOR="#111111" LOCALIZED_TEXT="AutomaticLayout.level,4">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node CREATED="1574057446666" ID="ID_776495852" MODIFIED="1621917974776" POSITION="right" TEXT="&#xbc84;&#xcd94;&#xc5b4; &#xbc15;&#xc2a4; &#xc124;&#xce58;">
+<node CREATED="1574062840958" ID="ID_585410409" MODIFIED="1621917969403" TEXT="&#xcc38;&#xc870;">
 <node CREATED="1574062850698" ID="ID_1214102242" LINK="https://websiteforstudents.com/install-opencart-on-ubuntu-16-04-lts-with-apache2-mariadb-and-php-7-1-support/" MODIFIED="1574062850698" TEXT="https://websiteforstudents.com/install-opencart-on-ubuntu-16-04-lts-with-apache2-mariadb-and-php-7-1-support/"/>
 <node COLOR="#3333ff" CREATED="1574062866105" ID="ID_1901246191" LINK="https://hostadvice.com/how-to/how-to-install-opencart-on-ubuntu-18-04-with-apache-mariadb-and-php-7/" MODIFIED="1574062867125" STYLE="bubble" TEXT="https://hostadvice.com/how-to/how-to-install-opencart-on-ubuntu-18-04-with-apache-mariadb-and-php-7/">
 <font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
@@ -12,16 +77,50 @@
 <node CREATED="1574057455303" ID="ID_1313785606" MODIFIED="1574057458502" TEXT="&#xc6b0;&#xbd84;&#xd2b8; &#xc124;&#xce58;"/>
 <node CREATED="1574057458743" ID="ID_1400032090" MODIFIED="1574057467431" TEXT="openCart&#xb77c;&#xb294; php &#xc124;&#xce58;"/>
 <node CREATED="1574057467600" ID="ID_636138252" MODIFIED="1574057468507" TEXT="&#xad6c;&#xc870; &#xbcf4;&#xae30;"/>
-<node CREATED="1574082126101" ID="ID_1422266145" MODIFIED="1574082129212" TEXT="&#xc591;&#xbc29;&#xd5a5; &#xc708;&#xb3c4;&#xc6b0;">
-<node CREATED="1574093044680" LINK="https://askubuntu.com/questions/22743/how-do-i-install-guest-additions-in-a-virtualbox-vm/22745#22745" MODIFIED="1574093044680" TEXT="https://askubuntu.com/questions/22743/how-do-i-install-guest-additions-in-a-virtualbox-vm/22745#22745"/>
+<node CREATED="1574082126101" ID="ID_1422266145" MODIFIED="1621917967525" TEXT="&#xc591;&#xbc29;&#xd5a5; &#xc708;&#xb3c4;&#xc6b0;">
+<node CREATED="1574093044680" ID="ID_1366006794" LINK="https://askubuntu.com/questions/22743/how-do-i-install-guest-additions-in-a-virtualbox-vm/22745#22745" MODIFIED="1574093044680" TEXT="https://askubuntu.com/questions/22743/how-do-i-install-guest-additions-in-a-virtualbox-vm/22745#22745"/>
 </node>
-<node CREATED="1574092590281" ID="ID_1873910075" MODIFIED="1574092594578" TEXT="&#xbaa8;&#xb4e0; &#xb85c;&#xcf00;&#xc774;&#xc158; 777">
-<node CREATED="1574092595052" LINK="https://stackoverflow.com/questions/8328481/chmod-777-to-a-folder-and-all-contents" MODIFIED="1574092595052" TEXT="https://stackoverflow.com/questions/8328481/chmod-777-to-a-folder-and-all-contents"/>
+<node CREATED="1574092590281" ID="ID_1873910075" MODIFIED="1621917967781" TEXT="&#xbaa8;&#xb4e0; &#xb85c;&#xcf00;&#xc774;&#xc158; 777">
+<node CREATED="1574092595052" ID="ID_332689569" LINK="https://stackoverflow.com/questions/8328481/chmod-777-to-a-folder-and-all-contents" MODIFIED="1574092595052" TEXT="https://stackoverflow.com/questions/8328481/chmod-777-to-a-folder-and-all-contents"/>
+</node>
+<node CREATED="1621918155909" ID="ID_1588529395" MODIFIED="1621918190047" TEXT="&#xbc84;&#xcd94;&#xc5b4;&#xbc15;&#xc2a4;&#xb85c; &#xc2dc;&#xc791;&#xd558;&#xb294; &#xacbd;&#xc6b0; &#xd3ec;&#xd2b8;&#xd3ec;&#xc6cc;&#xb529;&#xc744; &#xd574;&#xc918;&#xc57c;&#xd568;"/>
+</node>
+<node CREATED="1574215137840" ID="ID_1709275642" MODIFIED="1621918154833" POSITION="right" TEXT="&#xb9ac;&#xb205;&#xc2a4; find">
+<node CREATED="1574215227206" ID="ID_446564335" MODIFIED="1574215227206" TEXT="find / -name &#xd3f4;&#xb354;&#xba85; -type d"/>
+</node>
+<node CREATED="1580280082029" ID="ID_886773617" MODIFIED="1580280083248" POSITION="right" TEXT="&#xd3ec;&#xd2b8;&#xc5f4;&#xae30;">
+<node CREATED="1621942793987" ID="ID_537373711" MODIFIED="1621942797801" TEXT="&#xcc38;&#xc870;">
+<node CREATED="1621942798503" ID="ID_898455139" LINK="https://www.lesstif.com/system-admin/rhel-centos-firewall-22053128.html" MODIFIED="1621942798503" TEXT="https://www.lesstif.com/system-admin/rhel-centos-firewall-22053128.html"/>
+</node>
+<node CREATED="1621942799443" ID="ID_151130396" MODIFIED="1621943475867" TEXT="&#xac1c;&#xb150;">
+<node CREATED="1621944679354" ID="ID_822450961" MODIFIED="1621944690176" TEXT="zone&#xc774; &#xc788;&#xace0;, service&#xac00; &#xc788;&#xb2e4;."/>
+<node CREATED="1621944715898" ID="ID_1028492349" MODIFIED="1621944731563" TEXT="service&#xb294; zone&#xbc11;&#xc5d0; &#xc788;&#xc9c0;&#xb9cc; &#xb3c5;&#xb9bd;&#xb418;&#xc5b4; &#xc788;&#xb294; &#xb4ef; &#xd558;&#xace0;, &#xbaa8;&#xb4e0;&#xac8c; zone &#xc885;&#xc18d;&#xc774;&#xb2e4;."/>
+<node CREATED="1621945032886" ID="ID_284796942" MODIFIED="1621945037666" TEXT="zone&#xc740; public&#xc774; &#xac00;&#xc7a5; &#xc911;&#xc694;&#xd568;"/>
+</node>
+<node CREATED="1621944735569" ID="ID_828364507" MODIFIED="1621944887509" TEXT="&#xba85;&#xb839;&#xc5b4;">
+<node CREATED="1621944914173" ID="ID_1413717608" MODIFIED="1621944921353" TEXT="&#xbaa8;&#xb4e0; zone &#xb9ac;&#xc2a4;&#xd2b8;(&#xc758;&#xbbf8;&#xc5c6;&#xc74c;)">
+<node CREATED="1621944921729" ID="ID_111838633" MODIFIED="1621944921729" TEXT="firewall-cmd --list-all-zones"/>
+</node>
+<node CREATED="1621944887488" ID="ID_1787889935" MODIFIED="1621944898243" TEXT="&#xd65c;&#xc131;&#xd654;&#xb41c; zone &#xb9ac;&#xc2a4;&#xd2b8;">
+<node CREATED="1621944854150" ID="ID_872098809" MODIFIED="1621944854150" TEXT="firewall-cmd --get-active-zone"/>
+</node>
+<node CREATED="1621945000886" ID="ID_1367858703" MODIFIED="1621945007507" TEXT="&#xd574;&#xb2f9; zone&#xc758; service &#xb9ac;&#xc2a4;&#xd2b8;">
+<node CREATED="1621945007762" ID="ID_452576633" MODIFIED="1621945012994" TEXT="firewall-cmd --permanent --list-all --zone=zone&#xc774;&#xb984;"/>
+</node>
+<node CREATED="1621945224326" ID="ID_1163250345" MODIFIED="1621945229954" TEXT="&#xd574;&#xb2f9; zone&#xc758; service &#xcd94;&#xac00;">
+<node CREATED="1621945230352" ID="ID_1855835458" MODIFIED="1621945230352" TEXT="firewall-cmd --permanent --zone=dmz --add-service=http"/>
+<node CREATED="1621945242006" ID="ID_1250441242" MODIFIED="1621945242006" TEXT="firewall-cmd --permanent --zone=dmz --add-service=https"/>
+</node>
+<node CREATED="1621945109125" ID="ID_1684963198" MODIFIED="1621945136418" TEXT="&#xbc29;&#xd654;&#xbcbd; &#xd3ec;&#xd2b8; &#xd5c8;&#xb77d;(8080&#xd3ec;&#xd2b8;, public)">
+<node CREATED="1621945127383" ID="ID_337238271" MODIFIED="1621945127383" TEXT="firewall-cmd --permanent --zone=public --add-port=8080/tcp"/>
+<node CREATED="1621945179061" ID="ID_447696044" MODIFIED="1621945180810" TEXT="&#xbc94;&#xc704;">
+<node CREATED="1621945181032" ID="ID_882213491" MODIFIED="1621945181033" TEXT="firewall-cmd --permanent --zone=public --add-port=4000-4100/tcp"/>
+</node>
+<node CREATED="1621945188069" ID="ID_172865727" MODIFIED="1621945189506" TEXT="&#xc0ad;&#xc81c;">
+<node CREATED="1621945189752" ID="ID_763095240" MODIFIED="1621945189752" TEXT="firewall-cmd --permanent --zone=public --remove-port=8080/tcp"/>
 </node>
 </node>
-<node CREATED="1574215137840" ID="ID_1709275642" MODIFIED="1574215140066" POSITION="right" TEXT="&#xb9ac;&#xb205;&#xc2a4; find">
-<node CREATED="1574215227206" MODIFIED="1574215227206" TEXT="find / -name &#xd3f4;&#xb354;&#xba85; -type d"/>
 </node>
-<node CREATED="1580280082029" ID="ID_886773617" MODIFIED="1580280083248" POSITION="right" TEXT="&#xd3ec;&#xd2b8;&#xc5f4;&#xae30;"/>
+</node>
 </node>
 </map>
